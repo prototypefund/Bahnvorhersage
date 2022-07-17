62a9df541860d36b89a6bac7aaac33af588a2e62
McToel <theo.doellmann@gmx.de>
2022-07-17 14:57:35 +0200

    Moved Predictor into xgboost_multi_model.py

--- ml_models/xgboost_multi_model.py
+++ ml_models/xgboost_multi_model.py
@@ -4,12 +4,12 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import pickle
 from xgboost import XGBClassifier
 from sklearn.dummy import DummyClassifier
-from helpers import RtdRay
+from helpers import RtdRay, ttl_lru_cache
 import datetime
-import matplotlib.pyplot as plt
 import pandas as pd
 import dask.dataframe as dd
 from typing import Literal
+import numpy as np
 from config import JSON_MODEL_PATH, ENCODER_PATH
 
 
@@ -233,59 +233,212 @@ def test_models(n_models=15, **load_parameters):
         test_model(model, dp_test_x, test_y, model_name)
 
 
-def model_roc(model, x_test, y_test, model_name):
-    from sklearn.metrics import (
-        precision_recall_curve,
-        plot_precision_recall_curve,
-        auc,
-        roc_curve,
-    )
+class Predictor:
+    CATEGORICALS = ['o', 'c', 'n', 'station', 'pp']
 
-    prediction = model.predict_proba(x_test)[:, 1]
-    fpr, tpr, thresholds = roc_curve(y_test, prediction, pos_label=1)
-    roc_auc = auc(fpr, tpr)
+    def __init__(self, n_models: int = 15):
+        self.n_models = n_models
 
-    lw = 2
-    fig, ax = plt.subplots()
-    ax.plot(
-        fpr, tpr, color="darkorange", lw=lw, label="ROC curve (area = %0.2f)" % roc_auc
-    )
-    ax.plot([0, 1], [0, 1], color="navy", lw=lw, linestyle="--")
-    ax.set_xlim([0.0, 1.0])
-    ax.set_ylim([0.0, 1.05])
-    ax.set_xlabel("False Positive Rate")
-    ax.set_ylabel("True Positive Rate")
-    ax.set_title("Receiver operating characteristic model {}".format(model_name))
-    ax.legend(loc="lower right")
+    @ttl_lru_cache(seconds_to_live=60 * 60 * 4)
+    def categorical_encoder(
+        self, category: Literal['o', 'c', 'n', 'station', 'pp']
+    ) -> dict[str, int]:
+        """Load categorical encoder either from disk or time sensitive cache.
 
-    # fig1, ax1 = plt.subplots()
-    # ax1.set_title('Predictions')
-    # ax1.boxplot(prediction)
-    plt.show()
+        Parameters
+        ----------
+        category: str: `o` | `c` | `n` | `station` | `pp`
+            The category that should be encoded by the encoder
 
-    # plt.scatter(prediction, y_test, color='red', label='prediction')
-    # from sklearn import linear_model
-    # # Create linear regression object
-    # regr = linear_model.LinearRegression()
+        Returns
+        -------
+        dict[str, int]
+            The encoder dict to map any str / category to a int
 
-    # # Train the model using the training sets
-    # regr.fit(prediction.reshape(-1, 1), y_test)
-    # reg_y = regr.predict(prediction.reshape(-1, 1))
-    # plt.plot(prediction, reg_y, color='blue', linewidth=3)
+        Raises
+        ------
+        ValueError
+            The supplied category does not exist
+        """
+        if category not in self.CATEGORICALS:
+            raise ValueError(
+                f'category {category} is unknown. Valid categories are: {self.CATEGORICALS}'
+            )
+        return pickle.load(open(ENCODER_PATH.format(encoder=category), 'rb'))
 
-    # # plt.axis('tight')
-    # plt.axis('scaled')
-    # plt.legend()
+    @ttl_lru_cache(seconds_to_live=60 * 60 * 4)
+    @staticmethod
+    def model(minute: int, ar_or_dp: Literal['ar', 'dp']) -> XGBClassifier:
+        """Load trained XGBClassifier model for prediction either form disk or time sensitive cache. See `load_model` for more details."""
+        return load_model(minute, ar_or_dp)
 
-    # plt.tight_layout()
-    # plt.show()
+    def predict_ar(self, features: pd.DataFrame) -> np.ndarray:
+        features = features.to_numpy()
+        prediction = np.empty((len(features), self.n_models))
+        for model in range(self.n_models):
+            prediction[:, model] = Predictor.model(model, 'ar').predict_proba(
+                features, validate_features=False
+            )[:, 1]
+        return prediction
 
-    # from sklearn.metrics import precision_recall_curve
-    # from sklearn.metrics import plot_precision_recall_curve
+    def predict_dp(self, features: pd.DataFrame) -> np.ndarray:
+        features = features.to_numpy()
+        prediction = np.empty((len(features), self.n_models))
+        for model in range(self.n_models):
+            prediction[:, model] = Predictor.model(model, 'dp').predict_proba(
+                features, validate_features=False
+            )[:, 1]
+        return prediction
 
-    # disp = plot_precision_recall_curve(model, x_test, y_test)
-    # disp.ax_.set_title('2-class Precision-Recall curve')
-    # plt.show()
+    def predict_con(
+        self,
+        ar_prediction: np.ndarray,
+        dp_prediction: np.ndarray,
+        transfer_time: np.ndarray,
+    ) -> np.ndarray:
+        con_score = np.ones(len(transfer_time))
+
+        for tra_time in range(self.n_models):
+            mask = transfer_time == tra_time
+            if mask.any():
+                con_score[mask] = (
+                    ar_prediction[mask, max(tra_time - 2, 0)]
+                    * dp_prediction[mask, max(0, 2 - tra_time)]
+                )
+                con_score[mask] = con_score[mask] + np.sum(
+                    (
+                        ar_prediction[
+                            mask,
+                            max(tra_time - 2, 0)
+                            + 1 : dp_prediction.shape[1]
+                            - max(0, 2 - tra_time),
+                        ]
+                        - ar_prediction[
+                            mask,
+                            max(tra_time - 2, 0) : dp_prediction.shape[1]
+                            - 1
+                            - max(0, 2 - tra_time),
+                        ]
+                    )
+                    * dp_prediction[
+                        mask,
+                        max(0, 2 - tra_time)
+                        + 1 : dp_prediction.shape[1]
+                        + min(2 - tra_time, 0),
+                    ],
+                    axis=1,
+                )
+        return np.minimum(con_score, np.ones(len(con_score)))
+
+    def get_pred_data(
+        self, segments: list, streckennetz
+    ) -> tuple[pd.DataFrame, pd.DataFrame]:
+        dtypes = {
+            'station': 'int',
+            'lat': 'float',
+            'lon': 'float',
+            'o': 'int',
+            'c': 'int',
+            'n': 'int',
+            'distance_to_start': 'float',
+            'distance_to_end': 'float',
+            'pp': 'int',
+            'stop_id': 'int',
+            'minute': 'int',
+            'day': 'int',
+            'stay_time': 'float',
+        }
+        ar_data = pd.DataFrame(
+            columns=[
+                'station',
+                'lat',
+                'lon',
+                'o',
+                'c',
+                'n',
+                'distance_to_start',
+                'distance_to_end',
+                'pp',
+                'stop_id',
+                'minute',
+                'day',
+                'stay_time',
+            ],
+            index=range(len(segments)),
+        )
+        dp_data = ar_data.copy()
+        for i, segment in enumerate(segments):
+            # Encode categoricals
+            for cat in self.CATEGORICALS:
+                try:
+                    if cat == 'pp':
+                        ar_data.at[i, cat] = self.categorical_encoder(cat)[
+                            segment['ar_' + 'cp']
+                        ]
+                    else:
+                        ar_data.at[i, cat] = self.categorical_encoder(cat)[
+                            segment['ar_' + cat]
+                        ]
+                except KeyError:
+                    ar_data.at[i, cat] = -1
+                    print(
+                        'unknown {cat}: {value}'.format(
+                            cat=cat, value=segment['ar_' + cat]
+                        )
+                    )
+                try:
+                    if cat == 'pp':
+                        dp_data.at[i, cat] = self.categorical_encoder(cat)[
+                            segment['dp_' + 'cp']
+                        ]
+                    else:
+                        dp_data.at[i, cat] = self.categorical_encoder(cat)[
+                            segment['dp_' + cat]
+                        ]
+                except KeyError:
+                    dp_data.at[i, cat] = -1
+                    print(
+                        'unknown {cat}: {value}'.format(
+                            cat=cat, value=segment['dp_' + cat]
+                        )
+                    )
+
+            ar_data.at[i, 'lat'] = segment['ar_lat']
+            ar_data.at[i, 'lon'] = segment['ar_lon']
+            dp_data.at[i, 'lat'] = segment['dp_lat']
+            dp_data.at[i, 'lon'] = segment['dp_lon']
+
+            ar_data.at[i, 'stop_id'] = segment['ar_stop_id']
+            dp_data.at[i, 'stop_id'] = segment['dp_stop_id']
+
+            ar_data.at[i, 'distance_to_start'] = streckennetz.route_length(
+                segment['full_trip'][: ar_data.at[i, 'stop_id'] + 1],
+                date=segment['dp_pt'],
+            )
+            ar_data.at[i, 'distance_to_end'] = streckennetz.route_length(
+                segment['full_trip'][ar_data.at[i, 'stop_id'] :], date=segment['dp_pt']
+            )
+            dp_data.at[i, 'distance_to_start'] = streckennetz.route_length(
+                segment['full_trip'][: dp_data.at[i, 'stop_id'] + 1],
+                date=segment['dp_pt'],
+            )
+            dp_data.at[i, 'distance_to_end'] = streckennetz.route_length(
+                segment['full_trip'][dp_data.at[i, 'stop_id'] :], date=segment['dp_pt']
+            )
+
+            ar_data.at[i, 'minute'] = (
+                segment['ar_ct'].time().minute + segment['ar_ct'].time().hour * 60
+            )
+            ar_data.at[i, 'day'] = segment['ar_ct'].weekday()
+            dp_data.at[i, 'minute'] = (
+                segment['dp_ct'].time().minute + segment['dp_ct'].time().hour * 60
+            )
+            dp_data.at[i, 'day'] = segment['dp_ct'].weekday()
+
+            ar_data.at[i, 'stay_time'] = segment['stay_times'][ar_data.at[i, 'stop_id']]
+            dp_data.at[i, 'stay_time'] = segment['stay_times'][dp_data.at[i, 'stop_id']]
+
+        return ar_data.astype(dtypes), dp_data.astype(dtypes)
 
 
 if __name__ == "__main__":
