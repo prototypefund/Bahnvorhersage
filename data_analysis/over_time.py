c690238900cfb61337b5f1486da630b6b5952a1e
McToel <theo.doellmann@gmx.de>
2022-12-26 14:51:42 +0100

    Fancy new plots

--- data_analysis/over_time.py
+++ data_analysis/over_time.py
@@ -12,6 +12,7 @@ from helpers import RtdRay
 from config import n_dask_workers
 from database import cached_table_fetch
 from helpers import groupby_index_to_flat
+from typing import Literal, Optional
 
 
 def add_rolling_mean(df: pd.DataFrame, columns: list, window=3) -> pd.DataFrame:
@@ -44,7 +45,9 @@ def add_rolling_mean(df: pd.DataFrame, columns: list, window=3) -> pd.DataFrame:
     max_index = len(df) - 1
     dist_from_center = window // 2
     for i in range(dist_from_center):
-        df.loc[df.index[0] - df.index[i + 1] + df.index[0], :] = df.iloc[max_index - i, :]
+        df.loc[df.index[0] - df.index[i + 1] + df.index[0], :] = df.iloc[
+            max_index - i, :
+        ]
         df.loc[df.index[max_index] + (df.index[i + 1] - df.index[0]), :] = df.iloc[i, :]
     df = df.sort_index()
 
@@ -61,7 +64,7 @@ class OverTime(metaclass=ABCMeta):
             table_generator=self.generate_data,
             push=True,
             index_col='index',
-            **kwargs
+            **kwargs,
         )
 
     @property
@@ -116,7 +119,7 @@ class OverTime(metaclass=ABCMeta):
                 'dp_cs',
             ]
         )
-    
+
         rtd['ar_on_time'] = rtd['ar_delay'] <= 5
         rtd['dp_on_time'] = rtd['dp_delay'] <= 5
         rtd['ar_canceled'] = rtd['ar_cs'] == 'c'
@@ -124,43 +127,53 @@ class OverTime(metaclass=ABCMeta):
 
         # Use dask Client to do groupby as the groupby is complex and scales well on local cluster.
         from dask.distributed import Client
+
         with Client(n_workers=n_dask_workers, threads_per_worker=2) as client:
             rtd['agg_time'] = rtd['ar_pt'].fillna(value=rtd['dp_pt'])
             rtd['agg_time'] = rtd['agg_time'].dt.floor(self.resolution)
             rtd['agg_time'] = self.periodic_aggregator(rtd['agg_time'])
 
-            rtd = rtd.groupby('agg_time').agg({
-                'ar_delay': ['count', 'mean'],
-                'ar_happened': ['mean', 'sum'],
-                'ar_on_time': ['mean'],
-                'ar_canceled': ['mean'],
-                'dp_delay': ['count', 'mean'],
-                'dp_happened': ['mean', 'sum'],
-                'dp_on_time': ['mean'],
-                'dp_canceled': ['mean'],
-            }).compute()
+            rtd = (
+                rtd.groupby('agg_time')
+                .agg(
+                    {
+                        'ar_delay': ['count', 'mean'],
+                        'ar_happened': ['mean', 'sum'],
+                        'ar_on_time': ['mean'],
+                        'ar_canceled': ['mean'],
+                        'dp_delay': ['count', 'mean'],
+                        'dp_happened': ['mean', 'sum'],
+                        'dp_on_time': ['mean'],
+                        'dp_canceled': ['mean'],
+                    }
+                )
+                .compute()
+            )
             rtd = rtd.loc[~rtd.index.isna(), :]
             rtd = rtd.sort_index()
             rtd = groupby_index_to_flat(rtd)
 
-            rtd = rtd.astype({
-                'ar_delay_mean': 'float64',
-                'ar_delay_count': 'int',
-                'ar_happened_mean': 'float64',
-                'ar_happened_sum': 'int',
-                'ar_on_time_mean': 'float64',
-                'ar_canceled_mean': 'float64',
-                'dp_delay_mean': 'float64',
-                'dp_delay_count': 'int',
-                'dp_happened_mean': 'float64',
-                'dp_happened_sum': 'int',
-                'dp_on_time_mean': 'float64',
-                'dp_canceled_mean': 'float64',
-            })
+            rtd = rtd.astype(
+                {
+                    'ar_delay_mean': 'float64',
+                    'ar_delay_count': 'int',
+                    'ar_happened_mean': 'float64',
+                    'ar_happened_sum': 'int',
+                    'ar_on_time_mean': 'float64',
+                    'ar_canceled_mean': 'float64',
+                    'dp_delay_mean': 'float64',
+                    'dp_delay_count': 'int',
+                    'dp_happened_mean': 'float64',
+                    'dp_happened_sum': 'int',
+                    'dp_on_time_mean': 'float64',
+                    'dp_canceled_mean': 'float64',
+                }
+            )
 
             if self.rolling_mean_window > 0:
                 rtd = add_rolling_mean(
-                    rtd, [
+                    rtd,
+                    [
                         'ar_delay_mean',
                         'ar_delay_count',
                         'ar_happened_mean',
@@ -174,22 +187,41 @@ class OverTime(metaclass=ABCMeta):
                         'dp_on_time_mean',
                         'dp_canceled_mean',
                     ],
-                    window=self.rolling_mean_window
+                    window=self.rolling_mean_window,
                 )
 
             return rtd
 
     def plot(
-            self,
-            nticks=6,
-            kind: str = 'delay'
+        self,
+        nticks=6,
+        kind: Literal['delay', 'cancellations', 'on_time_percentage'] = 'delay',
+        save_as: Optional[str] = None,
     ):
+        """Plot the data.
+
+        Parameters
+        ----------
+        nticks : int, optional
+            Ticks on the y-axis, by default 6
+        kind : Literal["delay", "cancellations", "on_time_percentage"], optional
+            What to plot, by default 'delay'
+        save_as : Optional[str], optional
+            Whether and where to save plot to disk, by default None
+
+        Raises
+        ------
+        ValueError
+            kind incalid
+        """
         if kind not in ['delay', 'cancellations', 'on_time_percentage']:
-            raise ValueError(f'kind must be "delay", "cancellations" or "on_time_percentage" not {kind}')
+            raise ValueError(
+                f'kind must be "delay", "cancellations" or "on_time_percentage" not {kind}'
+            )
         fig, ax1 = plt.subplots()
         ax2 = ax1.twinx()
-        ax1.tick_params(axis="both", labelsize=20)
-        ax2.tick_params(axis="both", labelsize=20)
+        ax1.tick_params(axis='both', labelsize=20)
+        ax2.tick_params(axis='both', labelsize=20)
         ax1.xaxis.set_major_formatter(self.dt_formatter)
         ax1.xaxis.set_major_locator(self.dt_locator)
         ax2.xaxis.set_major_formatter(self.dt_formatter)
@@ -206,74 +238,97 @@ class OverTime(metaclass=ABCMeta):
 
         ax1.set_title(self.plot_title, fontsize=50)
         ax1.set_xlabel(self.plot_x_label, fontsize=30)
-        ax1.set_ylabel('Stops', color="blue", fontsize=30)
+        ax1.set_ylabel('Stops', color='blue', fontsize=30)
 
         if kind == 'delay':
-            ax1.plot(self.data['ar_happened_sum'] + self.data['dp_happened_sum'],
-                     color="blue",
-                     linewidth=3,
-                     label='Stops')
+            ax1.plot(
+                self.data['ar_happened_sum'] + self.data['dp_happened_sum'],
+                color='blue',
+                linewidth=3,
+                label='Stops',
+            )
+            ax1.set_ylim(bottom=0)
 
-            ax2.set_ylabel('Delay in Minutes', color="orange", fontsize=30)
-            ax2.plot(self.data['ar_delay_mean'],
-                     color="red",
-                     linewidth=3,
-                     label='Arrival delay')
-            ax2.plot(self.data['dp_delay_mean'],
-                     color="orange",
-                     linewidth=3,
-                     label='Departure delay')
+            ax2.set_ylabel('Delay in Minutes', color='orange', fontsize=30)
+            ax2.plot(
+                self.data['ar_delay_mean'],
+                color='red',
+                linewidth=3,
+                label='Arrival delay',
+            )
+            ax2.plot(
+                self.data['dp_delay_mean'],
+                color='orange',
+                linewidth=3,
+                label='Departure delay',
+            )
         elif kind == 'on_time_percentage':
-            ax1.plot(self.data['ar_happened_sum'] + self.data['dp_happened_sum'],
-                     color="blue",
-                     linewidth=3,
-                     label='Stops')
+            ax1.plot(
+                self.data['ar_happened_sum'] + self.data['dp_happened_sum'],
+                color='blue',
+                linewidth=3,
+                label='Stops',
+            )
 
-            ax2.set_ylabel('Stops on time (percent)', color="orange", fontsize=30)
-            ax2.plot((self.data['ar_on_time_mean']).round(3) * 100,
-                     color="red",
-                     linewidth=3,
-                     label='Arrival')
-            ax2.plot((self.data['dp_on_time_mean']).round(3) * 100,
-                     color="orange",
-                     linewidth=3,
-                     label='Departure')
+            ax2.set_ylabel('Stops on time (percent)', color='orange', fontsize=30)
+            ax2.plot(
+                (self.data['ar_on_time_mean']).round(3) * 100,
+                color='red',
+                linewidth=3,
+                label='Arrival',
+            )
+            ax2.plot(
+                (self.data['dp_on_time_mean']).round(3) * 100,
+                color='orange',
+                linewidth=3,
+                label='Departure',
+            )
             ax2.set_ylim(75, 100)
 
         elif kind == 'cancellations':
-            ax1.plot(self.data['ar_happened_sum'] + self.data['dp_happened_sum'],
-                     color="blue",
-                     linewidth=3,
-                     label='Stops')
+            ax1.plot(
+                self.data['ar_happened_sum'] + self.data['dp_happened_sum'],
+                color='blue',
+                linewidth=3,
+                label='Stops',
+            )
 
-            ax2.set_ylabel('Relative non-cancellations', color="orange", fontsize=30)
-            ax2.plot(self.data['ar_happened_mean'],
-                     color="red",
-                     linewidth=3,
-                     label='Arrival non-cancellations')
-            ax2.plot(self.data['dp_happened_mean'],
-                     color="orange",
-                     linewidth=3,
-                     label='Departure non-cancellations')
+            ax2.set_ylabel('Relative non-cancellations', color='orange', fontsize=30)
+            ax2.plot(
+                self.data['ar_happened_mean'],
+                color='red',
+                linewidth=3,
+                label='Arrival non-cancellations',
+            )
+            ax2.plot(
+                self.data['dp_happened_mean'],
+                color='orange',
+                linewidth=3,
+                label='Departure non-cancellations',
+            )
 
         fig.legend(fontsize=18)
         fig.autofmt_xdate()
-        plt.show()
+
+        if save_as:
+            fig.set_size_inches(13.6, 8.5)
+            fig.savefig(save_as, dpi=300, bbox_inches='tight')
+        else:
+            plt.show()
 
 
 class OverHour(OverTime):
     tablename = 'over_hour'
     resolution = '1min'
     rolling_mean_window = 3
-    dt_formatter = mdates.DateFormatter("%M")
+    dt_formatter = mdates.DateFormatter('%M')
     dt_locator = mdates.MinuteLocator(byminute=range(0, 60, 10))
     plot_title = 'Stops over the hour'
     plot_x_label = 'Minute'
 
     def periodic_aggregator(self, rtd: dd.Series) -> dd.Series:
-        return (
-            datetime.datetime(year=2000, month=1, day=3)
-            + dd.to_timedelta(rtd.dt.minute, unit='m')
+        return datetime.datetime(year=2000, month=1, day=3) + dd.to_timedelta(
+            rtd.dt.minute, unit='m'
         )
 
 
@@ -281,7 +336,7 @@ class OverDay(OverTime):
     tablename = 'over_day'
     resolution = '10min'
     rolling_mean_window = 5
-    dt_formatter = mdates.DateFormatter("%H:%M")
+    dt_formatter = mdates.DateFormatter('%H:%M')
     dt_locator = mdates.HourLocator()
     plot_title = 'Stops over the day'
     plot_x_label = 'Time'
@@ -298,7 +353,7 @@ class OverWeek(OverTime):
     tablename = 'over_week'
     resolution = '10min'
     rolling_mean_window = 5
-    dt_formatter = mdates.DateFormatter("%A %H:%M")  # E.g.: Monday 08:00
+    dt_formatter = mdates.DateFormatter('%A %H:%M')  # E.g.: Monday 08:00
     dt_locator = mdates.HourLocator(interval=12)
     plot_title = 'Stops over the week'
     plot_x_label = 'Day'
@@ -316,7 +371,7 @@ class OverYear(OverTime):
     tablename = 'overyear'
     resolution = '1D'
     rolling_mean_window = 0
-    dt_formatter = mdates.DateFormatter("%b %Y")
+    dt_formatter = mdates.DateFormatter('%b %Y')
     dt_locator = mdates.MonthLocator()
     plot_title = 'Stops over the year'
     plot_x_label = 'Month'
@@ -329,7 +384,7 @@ class OverYearWeekly(OverTime):
     tablename = 'over_year_weekly'
     resolution = '7D'
     rolling_mean_window = 0
-    dt_formatter = mdates.DateFormatter("%b %Y")
+    dt_formatter = mdates.DateFormatter('%b %Y')
     dt_locator = mdates.MonthLocator()
     plot_title = 'Stops over the year'
     plot_x_label = 'Month'
@@ -341,26 +396,22 @@ class OverYearWeekly(OverTime):
 if __name__ == '__main__':
     import helpers.bahn_vorhersage
 
-    # print('grouping over hour')
-    # time = OverHour(generate=False)
-    # time.plot()
+    print('grouping over hour')
+    time = OverHour(generate=False)
+    time.plot(save_as='over_hour_cancellations.png', kind='cancellations')
 
-    # print('grouping over day')
-    # time = OverDay(generate=False)
-    # time.plot()
+    print('grouping over day')
+    time = OverDay(generate=False)
+    time.plot(save_as='over_day_cancellations.png', kind='cancellations')
 
-    # print('grouping over week')
-    # time = OverWeek(generate=False)
-    # time.plot()
+    print('grouping over week')
+    time = OverWeek(generate=False)
+    time.plot(save_as='over_week_cancellations.png', kind='cancellations')
 
     print('grouping over year')
     time = OverYear(generate=False)
-    time.plot(kind='on_time_percentage')
+    time.plot(save_as='over_year_cancellations.png', kind='cancellations')
 
     print('grouping over year')
     time = OverYearWeekly(generate=False)
-    time.plot(kind='delay')
-
-    print('grouping over year')
-    time = OverYearWeekly(generate=False)
-    time.plot(kind='cancellations')
+    time.plot(save_as='over_year_weekly_cancellations.png', kind='cancellations')
