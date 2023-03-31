e2aae08e15b2a3ef2c8d3e0d0843e19ef3657579
McToel <theo.doellmann@gmx.de>
2023-03-31 13:49:57 +0200

    Dropped table

--- database/plan.py
+++ database/plan.py
@@ -1,3 +1,5 @@
+# The table in the database was dropped on 31. März 2023 and does only exist in backups
+
 import os
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
