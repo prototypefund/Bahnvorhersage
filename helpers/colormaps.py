701f07e87e7c0f847a60d307003b3788d994c931
McToel <theo.doellmann@gmx.de>
2023-07-11 16:56:33 +0200

    added colormap

--- helpers/colormaps.py
+++ helpers/colormaps.py
@@ -2,3 +2,4 @@ import seaborn as sns
 from matplotlib.colors import ListedColormap
 
 paired = lambda n_colors: ListedColormap(sns.color_palette('Paired', n_colors=n_colors))
+seaborn_colors = lambda n_colors: ListedColormap(sns.color_palette(n_colors=n_colors))
