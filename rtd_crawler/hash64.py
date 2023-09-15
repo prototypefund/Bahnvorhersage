ac7f20a90e53000c6eef7a663df13b0776870120
McToel <theo.doellmann@gmx.de>
2023-09-15 21:45:54 +0200

    slightly faster hash without conversion mistake

--- rtd_crawler/hash64.py
+++ rtd_crawler/hash64.py
@@ -1,4 +1,5 @@
 from cityhash import CityHash64
+import xxhash
 
 
 def hash64(to_hash: str) -> int:
@@ -16,3 +17,20 @@ def hash64(to_hash: str) -> int:
         Hashed input as int64
     """
     return CityHash64(to_hash) - ((2 ** 63) - 1)
+
+
+def xxhash64(to_hash: str) -> int:
+    """
+    Create int64 hash from input. Compatible with postgres BIGINT as the hash is signed.
+
+    Parameters
+    ----------
+    to_hash : str
+        String to hash
+
+    Returns
+    -------
+    int
+        Hashed input as int64
+    """
+    return xxhash.xxh3_64_intdigest(to_hash) - ((2 ** 63))
